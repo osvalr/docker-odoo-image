72d100a5adab5ff12478ed00ecb1c3cf2ff9c2a0
Shippable <hello@shippable.com>
2015-11-12 14:44:17 +0000

    [REF] dlint: add only 7 chars for rev sha

--- .dlint.sh
+++ .dlint.sh
@@ -7,7 +7,7 @@ if [ -z ${REPO} ] || [ -z ${VERSION} ]; then
 	exit 1
 fi
 
-for dfile in $(git diff --name-status ${VERSION}..$(git rev-parse HEAD) | grep Dockerfile | grep -E '^(A|M|R){1}.*$' | awk '{ print $2 }'); do 
+for dfile in $(git diff --name-status ${VERSION}..$(git rev-parse HEAD | cut -c 1-7) | grep Dockerfile | grep -E '^(A|M|R){1}.*$' | awk '{ print $2 }'); do 
 	if ! [ -f $dfile ]; then
 		exit 1
 	fi
